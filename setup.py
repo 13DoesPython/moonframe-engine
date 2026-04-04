e40bbb1121abfc428eeea39ee12c7f4c2b320519
13DoesPython <riyazsamin@gmail.com>
2026-04-04 12:46:44 +0200

    Updated README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="moonframe-engine",
-    version="1.1.0",
+    version="1.1.1",
     author="Samin",
     description="A lightweight 2D game engine built on Pyglet",
     long_description=long_description,
